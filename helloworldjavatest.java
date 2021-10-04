ae337822a175c57be8d95893466a9f00b524cfb4
sentbala <88264251+sentbala@users.noreply.github.com>
2021-10-04 15:51:12 +0400

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -2,6 +2,6 @@ public class helloworldjavatest{
 
 	public static void main(String[] args){
 		
-		System.out.println("Hello World - Polling Demo");
+		System.out.println("Hello World - Polling Demo - 2nd try");
 	}
 }
